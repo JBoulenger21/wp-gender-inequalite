95b93883332af5a8a3b33e3619f62499e7cc8127
mathieuradjouki <m.radjouki@codeur.online>
2020-10-15 15:33:31 +0200

    add stylesheet in head and function

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <title></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
+    <?php wp_head(); ?>
+  </head>
+  <body>
+
+    <h1>test du header pour voir si ça marche, si ça marche pas c'est la merde</h1>
--- page.php
+++ page.php
@@ -1,3 +1,5 @@
 <?php get_header(); ?>
-    
+
+<h2>Welcome here</h2>
+
 <?php get_footer(); ?>
