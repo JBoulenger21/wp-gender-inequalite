c78caf529bcd9b47e67e2aeb04dc4efe3a304a44
mathieuradjouki <m.radjouki@codeur.online>
2020-10-15 16:56:16 +0200

    recuperation develop

--- functions.php
+++ functions.php
@@ -1,17 +1,20 @@
 <?php
-/**
-** activation theme
-**/
-// add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-// function theme_enqueue_styles() {
-//  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-//
-// }
-add_theme_support('title-tag');
+
+function montheme_supports(){
+  add_theme_support('title-tag');
+
+}
 
 function montheme_register_assets()
 {
+  wp_register_style('bootstrap','https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css', []);
+  wp_register_script('bootstrap','https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', ['popper'], false, true);
+  wp_register_script('popper','https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js', [], false, true);
+  wp_enqueue_style('bootstrap');
+  wp_enqueue_script('bootstrap');
   wp_enqueue_style('style',get_stylesheet_uri());
 }
 
+
+add_action('after_setup_theme','montheme_supports');
 add_action('wp_enqueue_scripts', 'montheme_register_assets');
