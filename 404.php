ba22ba52ed839d85d8df4b405523c7b2691923d0
JBoulenger21 <boulenger.julie@gmail.com>
2020-10-16 15:20:44 +0200

    page 404 modifiée

--- 404.php
+++ 404.php
@@ -1,7 +1,6 @@
-<?php get_header() ?>
+<?php get_header('404') ?>
 
-<div class="container contain-404">
-  <h2 class="center-title-404"><?php _e('Oops !') ?></h2>
-  <p class="center-content-404"><?php _e("Page non trouvée. Retournez à l'accueil.") ?></p>
-</div>
+
+
+
 <?php get_footer() ?>
---
+++ header-404.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <title><?php wp_title(); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
+    <?php wp_head(); ?>
+  </head>
+  <body>
+
+    <header class="header">
+      <div class="container">
+        <nav class="navigation">
+          <div class="logo">
+            <a href="<?php echo home_url( '/' ); ?>">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.white.png" alt="icone logo">
+            </a>
+          </div>
+          <div class="item-navigation">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a>
+                  <?php wp_nav_menu( array (
+                  'theme_location' => 'navigation-hf',
+                  'menu_class' => 'menu_navigation',
+                ) ); ?>
+                </a>
+              </li>
+            </ul>
+
+          </div>
+
+
+        </nav>
+        <div class="container contain-404">
+          <h1>Oops<span class="bidouille">_</span>!</h1>
+          <h2>Page non trouvée. Retournez à l'accueil.</h2>
+        </div>
+
+      </div>
+
+    </header>
