52624c0d9375cfdde212289d0871e6e6030f07d8
JBoulenger21 <boulenger.julie@gmail.com>
2020-10-16 13:06:45 +0200

    Page 404 mise en forme

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php get_header() ?>
 
-<div class="container">
-  <h2><?php _e('Oops !') ?></h2>
-  <p><?php _e("Page non trouvée. Retournez à l'accueil.") ?></p>
+<div class="container contain-404">
+  <h2 class="center-title-404"><?php _e('Oops !') ?></h2>
+  <p class="center-content-404"><?php _e("Page non trouvée. Retournez à l'accueil.") ?></p>
 </div>
 <?php get_footer() ?>
