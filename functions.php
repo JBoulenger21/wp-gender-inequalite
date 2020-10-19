29a5a9039d8902905152448393a02f7e0252ada9
laurinehrd <laurine.hrd@hotmail.fr>
2020-10-19 10:57:42 +0200

    cpt and modif css

--- functions.php
+++ functions.php
@@ -23,8 +23,64 @@ function register_my_menu() {
 register_nav_menu('navigation-hf',__('navigation-hf'));
 }
 
+function sections_custom_post_type() {
+	$labels = array(
+		'name'                => 'sections',
+		'singular_name'       => 'sections',
+		'menu_name'           => 'sections',
+		'all_items'           => 'Toutes les sections',
+		'view_item'           => 'Voir les sections',
+		'add_new_item'        => 'Ajouter une nouvelle section',
+		'add_new'             => 'Ajouter',
+		'edit_item'           => 'Editer la section',
+		'update_item'         => 'Modifier la section',
+		'search_items'        => 'Rechercher une section',
+		'not_found'           => 'Non trouvée',
+		'not_found_in_trash'  => 'Non trouvée dans la corbeille',
+	);
+	$args = array(
+		'label'               => 'sections',
+		'description'         => 'Tous sur les évènements',
+		'labels'              => $labels,
+    'menu_icon'           => 'dashicons-info',
+    'menu_position'       => 5,
+		'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+		'show_in_rest'        => true,
+		'hierarchical'        => false,
+		'public'              => true,
+		'has_archive'         => true,
+		'rewrite'			        => array( 'slug' => 'information'),
+	);
+	register_post_type( 'sections', $args );
+}
+
+function cause_custom_post_types() {
+  // CPT causes conséquences
+  $labels = array(
+      'name' => 'Cause Consequence',
+      'all_items' => 'Causes Consequences',  // affiché dans le sous menu
+      'singular_name' => 'Cause',
+      'add_new_item' => 'Ajouter une cause consequence',
+      'edit_item' => 'Modifier la cause consequence',
+      'menu_name' => 'Cause Consequence'
+  );
+  $args = array(
+      'labels' => $labels,
+      'public' => true,
+      'show_in_rest' => true,
+      'has_archive' => true,
+      'supports' => array( 'title', 'editor','thumbnail' ),
+      'menu_position' => 5,
+      'menu_icon' => 'dashicons-admin-customizer',
+  );
+  register_post_type( 'cause', $args );
+}
+
 
 add_action('after_setup_theme','montheme_supports');
 add_action('wp_enqueue_scripts', 'montheme_register_assets');
 add_filter('document_title_separator', 'montheme_title_separator');
 add_action( 'init', 'register_my_menu' );
+add_action( 'init', 'sections_custom_post_type' );
+add_action( 'init', 'cause_custom_post_types' );
+
