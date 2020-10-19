858ed427bf35e3f8ec9f4c3df0f21ae5c96e3342
JBoulenger21 <boulenger.julie@gmail.com>
2020-10-19 11:00:37 +0200

    none

--- functions.php
+++ functions.php
@@ -23,7 +23,6 @@ function register_my_menu() {
 register_nav_menu('navigation-hf',__('navigation-hf'));
 }
 
-
 add_action('after_setup_theme','montheme_supports');
 add_action('wp_enqueue_scripts', 'montheme_register_assets');
 add_filter('document_title_separator', 'montheme_title_separator');
