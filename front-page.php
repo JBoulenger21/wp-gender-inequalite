66e3f24d5d6d37f331433f4e5d1e3496cb5c8091
laurinehrd <laurine.hrd@hotmail.fr>
2020-10-19 12:05:56 +0200

    add cpt

--- front-page.php
+++ front-page.php
@@ -18,32 +18,24 @@
     <div class="pages-home">
         <h1>Pages</h1>
         <div class="row">
+
+        <?php 
+        query_posts(array(
+        'post_type' => 'sections',
+        'showposts' => 4
+        ) );
+        ?>
+            <?php while (have_posts()) : the_post(); ?>
+
             <div class="col-lg-6 text-center">
-                <h2>Données chiffrées</h2>
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/girlspeak.png" alt="image d'exemple">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <h2><?php the_title(); ?></h2>
+                <?php the_post_thumbnail('post-thumbnail', ['class' => 'img-page-home img-fluid', 'alt' => 'icone section']); ?>
+                <p><?php the_content(); ?></p>
                 <button type="button" class="btn btn-lg btn-primary">Voir</button>
             </div>
-            <div class="col-lg-6 text-center">
-                <h2>Fondements juridiques</h2>
-                <img src="" alt="">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <button type="button" class="btn btn-lg btn-primary">Voir</button>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-6 text-center">
-                <h2>Causes et conséquences</h2>
-                <img src="" alt="">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <button type="button" class="btn btn-lg btn-primary">Voir</button>
-            </div>
-            <div class="col-lg-6 text-center">
-                <h2>Infographies</h2>
-                <img src="" alt="">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <button type="button" class="btn btn-lg btn-primary">Voir</button>
-            </div>
+
+        <?php endwhile; ?>
+
         </div>
     </div>
 
