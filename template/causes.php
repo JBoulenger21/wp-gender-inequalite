5aface4e643e74d65d01c9d40ef1fea27bf713cb
JBoulenger21 <boulenger.julie@gmail.com>
2020-10-19 16:44:58 +0200

    essai de responsive

--- template/causes.php
+++ template/causes.php
@@ -15,13 +15,16 @@ get_header();
   ) );
 $i = 0;
  while (have_posts()) : the_post();
+ ?>
+<div class="causeDesktop">
+ <?php
 foreach ($cause as $post) {
   $i++;
   if ($i % 2 == 0){
 ?>
 <div class="row cause">
   <div class="col-lg-6 col-sm-12">
-      <?php the_post_thumbnail('medium',[ 'alt' => '']) ?>
+      <?php the_post_thumbnail('thumbnail',[ 'alt' => '']) ?>
   </div>
   <div class="col-lg-6 col-sm-12">
       <h3><?php the_title(); ?></h3>
@@ -48,8 +51,26 @@ foreach ($cause as $post) {
 <?php
 }
 }
- ?>
+endwhile;
 
+query_posts(array(
+'post_type' => 'cause',
+'showposts' => 3,
+) );
+while (have_posts()) : the_post();
+ ?>
+</div>
+<div class="causeResponsive">
+  <div class="row cause">
+    <div class="col-12">
+        <h3><?php the_title(); ?></h3>
+        <div class="causeContent">
+         <?php the_content(); ?>
+        </div>
+        <?php the_post_thumbnail('medium',[ 'alt' => '']) ?>
+    </div>
+  </div>
+</div>
 
 
 <?php endwhile; ?>
