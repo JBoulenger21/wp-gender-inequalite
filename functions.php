9c97d9e06b5add40b624f0c1d74d2508b3b28171
laurinehrd <laurine.hrd@hotmail.fr>
2020-10-19 15:27:32 +0200

    add cpt for section presentation front-page

--- functions.php
+++ functions.php
@@ -23,6 +23,37 @@ function register_my_menu() {
 register_nav_menu('navigation-hf',__('navigation-hf'));
 }
 
+function presentation_custom_post_type() {
+	$labels = array(
+		'name'                => 'presentation',
+		'singular_name'       => 'presentation',
+		'menu_name'           => 'presentation',
+		'all_items'           => 'Toutes les presentations',
+		'view_item'           => 'Voir les presentations',
+		'add_new_item'        => 'Ajouter une nouvelle presentation',
+		'add_new'             => 'Ajouter',
+		'edit_item'           => 'Editer la presentation',
+		'update_item'         => 'Modifier la presentation',
+		'search_items'        => 'Rechercher une presentation',
+		'not_found'           => 'Non trouvée',
+		'not_found_in_trash'  => 'Non trouvée dans la corbeille',
+	);
+	$args = array(
+		'label'               => 'presentation',
+		'description'         => 'Tous sur les évènements',
+		'labels'              => $labels,
+    'menu_icon'           => 'dashicons-edit-large',
+    'menu_position'       => 5,
+		'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+		'show_in_rest'        => true,
+		'hierarchical'        => false,
+		'public'              => true,
+		'has_archive'         => true,
+		'rewrite'			        => array( 'slug' => 'information'),
+	);
+	register_post_type( 'presentation', $args );
+}
+
 function sections_custom_post_type() {
 	$labels = array(
 		'name'                => 'sections',
@@ -83,4 +114,5 @@ add_filter('document_title_separator', 'montheme_title_separator');
 add_action( 'init', 'register_my_menu' );
 add_action( 'init', 'sections_custom_post_type' );
 add_action( 'init', 'cause_custom_post_types' );
+add_action( 'init', 'presentation_custom_post_type' );
 
