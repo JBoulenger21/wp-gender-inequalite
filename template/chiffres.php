c28292cbad12b6a2ffd890786011b7433b9a63f8
mathieuradjouki <m.radjouki@codeur.online>
2020-10-20 09:53:00 +0200

    refactorisation css chiffres

--- template/chiffres.php
+++ template/chiffres.php
@@ -24,24 +24,34 @@ get_header();
     <div class="details">
         <h3>Plus de détails</h3>
         <div class="articles-details">
-            <div class="cat">
-                <div class="cat-tableau">
+            <div class="cat tab-left">
+                <div class="img-tableau">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cat-social.png" alt="tableau des écart de rémunération selon catégories social">
                 </div>
-                <div class="social-article">
+                <div class="article">
                     <h4>Les catégories sociales</h4>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio eligendi maxime facere excepturi, ullam inventore saepe, earum quos non officiis quidem, quisquam tempora nesciunt. In eligendi laborum doloribus ea explicabo! Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit voluptas minima aperiam neque, ab placeat quis dolorem eligendi? Voluptate nesciunt sequi eligendi incidunt reiciendis voluptatum autem. Magni obcaecati laborum dicta!</p>
                     <a href="https://www.inegalites.fr/Les-inegalites-de-salaires-entre-les-femmes-et-les-hommes-etat-des-lieux?id_theme=22" target="_blank" rel="noopener" class="sources">(sources)</a>
                 </div>
             </div>
-            <div class="ecart-niveau">              
-                <div class="niveau-article">
+            <div class="ecart-niveau tab-right">              
+                <div class="article">
                     <h4>Les niveaux de rémunération</h4>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio eligendi maxime facere excepturi, ullam inventore saepe, earum quos non officiis quidem, quisquam tempora nesciunt. In eligendi laborum doloribus ea explicabo! Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem veritatis cum animi harum nisi ab id doloribus fuga pariatur sapiente numquam molestias eligendi voluptates praesentium velit reiciendis est, modi expedita. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium adipisci harum tenetur fuga quam dolor sequi at vel eligendi optio omnis eveniet, error enim, velit debitis, assumenda veniam tempora veritatis.</p>
                     <a href="https://www.inegalites.fr/Les-inegalites-de-salaires-entre-les-femmes-et-les-hommes-etat-des-lieux?id_theme=22" target="_blank" rel="noopener" class="sources">(sources)</a>
                 </div>
-                <div class="niveau-tableau">
+                <div class="img-tableau">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/dif-sexe-niveau.png" alt="tableau des écart de rémunération selon catégories social">
+                </div>
+            </div>
+            <div class="repartition tab-left">
+                <div class="img-tableau">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/repartition.png" alt="tableau de la répartition des écarts de rémunération">
+                </div>
+                <div class="article">
+                    <h4>Répartition des inégalitées</h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio eligendi maxime facere excepturi, ullam inventore saepe, earum quos non officiis quidem, quisquam tempora nesciunt. In eligendi laborum doloribus ea explicabo! Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit voluptas minima aperiam neque, ab placeat quis dolorem eligendi? Voluptate nesciunt sequi eligendi incidunt reiciendis voluptatum autem. Magni obcaecati laborum dicta!</p>
+                    <a href="https://www.inegalites.fr/Les-inegalites-de-salaires-entre-les-femmes-et-les-hommes-etat-des-lieux?id_theme=22" target="_blank" rel="noopener" class="sources">(sources)</a>
                 </div>
             </div>
         </div>
