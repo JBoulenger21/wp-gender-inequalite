752ed4c641a1534c9124ab2f8adcd4f7d1bba853
mathieuradjouki <m.radjouki@codeur.online>
2020-10-20 11:07:06 +0200

    commit avant push sur develop

--- template/chiffres.php
+++ template/chiffres.php
@@ -22,13 +22,11 @@ get_header();
         <?php while (have_posts()) : the_post(); ?>
 
         <div class="base-salaire">
-            <?php the_content(); ?>
-            <!-- <p>En "ETP" [1], les femmes touchent 18,5 % de moins que les hommes, selon l’Insee. La discrimination pure serait d’environ 10 % d’après le ministère du Travail. Le salaire mensuel net moyen des hommes, ETP  est de 2 438 € en 2015, celui des femmes de 1 986 €, soit un écart de 452 €. Les femmes perçoivent donc, en moyenne, 81,5 % du salaire des hommes. Soit un salire inférieur de 18,5%. Ou encore les  hommes touchent 22,8% de plus.</p> -->
+            <?php the_content(); ?>          
             <a href="https://www.inegalites.fr/Les-inegalites-de-salaires-entre-les-femmes-et-les-hommes-etat-des-lieux?id_theme=22" target="_blank" rel="noopener" class="sources">(sources)</a>
         </div>
         <div class="tableau-base">
         <?php the_post_thumbnail('post-thumbnail', ['class' => 'tableau-base img-fluid', 'alt' => '']); ?>
-            <!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/images/salaire-net-moyen-selon.jpeg" alt="Tableau representant le salaire net moyen (source : INSEE)"> -->
         </div>
             <?php endwhile; ?>
     </div>
