851b823fca232dfb3794ac0d8aa8a9c44d8785f9
mathieuradjouki <m.radjouki@codeur.online>
2020-10-20 14:37:17 +0200

    add functions for dynamic page

--- functions.php
+++ functions.php
@@ -159,12 +159,12 @@ function cause_custom_post_types() {
   );
   register_post_type( 'cause', $args );
 }
-function chiffre_base_custom_post_type() {
+function chiffre_custom_post_type() {
 	$labels = array(
 		'name'                => 'Données chiffrées',
 		'singular_name'       => 'Données chiffrées',
 		'menu_name'           => 'Données chiffrées',
-		'all_items'           => 'Données chiffrées',
+		'all_items'           => 'introductions',
 		'view_item'           => 'Voir Données chiffrées',
 		'add_new_item'        => 'Ajouter une nouvelle donnée',
 		'add_new'             => 'Ajouter',
@@ -187,7 +187,7 @@ function chiffre_base_custom_post_type() {
 		'has_archive'         => true,
 		'rewrite'			        => array( 'slug' => 'information'),
 	);
-	register_post_type( 'chiffre_base', $args );
+	register_post_type( 'chiffre', $args );
 }
 
 function consequence_custom_post_types() {
@@ -212,6 +212,37 @@ function consequence_custom_post_types() {
   register_post_type( 'consequence', $args );
 }
 
+function detail_custom_post_type() {
+	$labels = array(
+		'name'                => 'Plus de détails',
+		'singular_name'       => 'chiffres détaillés',
+		'menu_name'           => 'chiffres détaillés',
+		'all_items'           => 'Chiffres en détails',
+		'view_item'           => 'Voir les détails',
+		'add_new_item'        => 'Ajouter un nouvel article',
+		'add_new'             => 'Ajouter',
+		'edit_item'           => 'Editer un article',
+		'update_item'         => 'Modifier un article',
+		'search_items'        => 'Rechercher un article',
+		'not_found'           => 'Non trouvée',
+		'not_found_in_trash'  => 'Non trouvée dans la corbeille',
+	);
+	$args = array(
+		'label'               => 'chiffres détaillés',
+		'description'         => 'Tous  les chiffres',
+		'labels'              => $labels,
+    'menu_icon'           => 'dashicons-edit-large',
+    'menu_position'       => 5,
+		'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+		'show_in_rest'        => true,
+		'hierarchical'        => false,
+		'public'              => true,
+		'has_archive'         => true,
+		'rewrite'			        => array( 'slug' => 'information'),
+	);
+	register_post_type( 'detail', $args );
+}
+
 add_filter( 'excerpt_length', function () {
 	return 90;
 }, 999 );
@@ -224,6 +255,7 @@ add_action( 'init', 'cause_custom_post_types' );
 add_action( 'init', 'consequence_custom_post_types' );
 add_action( 'init', 'presentation_custom_post_type' );
 add_action( 'init', 'introduction_custom_post_type' );
-add_action( 'init', 'chiffre_base_custom_post_type' );
+add_action( 'init', 'chiffre_custom_post_type' );
+add_action( 'init', 'detail_custom_post_type' );
 add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
 add_action( 'after_setup_theme', 'themename_custom_header_setup' );
