68315897d28ac1417728df38253fde084a24e24f
JBoulenger21 <boulenger.julie@gmail.com>
2020-10-27 11:42:00 +0100

    essai de centrage

--- template/cause.php
+++ template/cause.php
@@ -4,167 +4,171 @@ Template Name: Causes
 */
 get_header();
 ?>
-<div class="container">
-<h1>Causes</h1>
 
-<?php
-  $cause = get_posts(array(
-  'post_type' => 'cause',
-  'showposts' => 3,
-  'numberposts' => 3,
-  'order' => 'ASC'
-  ) );
-$i = 0;
-while (have_posts()) : the_post();
- ?>
-<div class="causeDesktop">
- <?php
+<div class="centerCause">
+  <div class="container">
+    <h1>Causes</h1>
 
-foreach ($cause as $post) {
+    <?php
+      $cause = get_posts(array(
+      'post_type' => 'cause',
+      'showposts' => 3,
+      'numberposts' => 3,
+      'order' => 'ASC'
+      ) );
+    $i = 0;
+    while (have_posts()) : the_post();
+     ?>
+    <div class="causeDesktop">
+     <?php
 
-  $i++;
-  if ($i % 2 == 0){
-?>
-<div class="row cause">
-  <div class="col-lg-6 col-sm-12">
-      <?php the_post_thumbnail('medium',[ 'alt' => '']) ?>
-  </div>
-  <div class="col-lg-6 col-sm-12">
-      <h3><?php the_title(); ?></h3>
-      <div class="causeContent">
-       <?php the_excerpt(); ?>
+    foreach ($cause as $post) {
+
+      $i++;
+      if ($i % 2 == 0){
+    ?>
+    <div class="row cause">
+      <div class="col-lg-6 col-sm-12">
+          <?php the_post_thumbnail('medium',[ 'alt' => '']) ?>
       </div>
-  </div>
-</div>
+      <div class="col-lg-6 col-sm-12 childcenter">
+          <h3><?php the_title(); ?></h3>
+          <div class="causeContent">
+           <?php the_excerpt(); ?>
+          </div>
+      </div>
+    </div>
 
-<?php
-} else{
-  ?>
-  <div class="row cause">
-    <div class="col-lg-6 col-sm-12">
-        <h3><?php the_title(); ?></h3>
-        <div class="causeContent">
-         <?php the_excerpt(); ?>
+    <?php
+    } else{
+      ?>
+      <div class="row cause">
+        <div class="col-lg-6 col-sm-12">
+            <h3><?php the_title(); ?></h3>
+            <div class="causeContent">
+             <?php the_excerpt(); ?>
+            </div>
         </div>
+        <div class="col-lg-6 col-sm-12 childcenter">
+            <?php the_post_thumbnail('medium',[ 'alt' => '']) ?>
+        </div>
+      </div>
+    <?php
+    }
+    }
+
+    endwhile;
+    ?>
     </div>
-    <div class="col-lg-6 col-sm-12">
-        <?php the_post_thumbnail('medium',[ 'alt' => '']) ?>
-    </div>
+
+    <?php
+    query_posts(array(
+    'post_type' => 'cause',
+    'showposts' => 3,
+    'numberposts' => 3,
+    'order' => 'ASC'
+    ) );
+    while (have_posts()) : the_post();
+     ?>
   </div>
-<?php
-}
-}
-
-endwhile;
-?>
-</div>
-
-<?php
-query_posts(array(
-'post_type' => 'cause',
-'showposts' => 3,
-'numberposts' => 3,
-'order' => 'ASC'
-) );
-while (have_posts()) : the_post();
- ?>
 </div>
 <div class="causeResponsive">
-  <div class="row cause">
-    <div class="col-12">
-        <h3><?php the_title(); ?></h3>
-        <div class="causeContent">
-         <?php the_content(); ?>
+      <div class="row cause">
+        <div class="col-12">
+            <h3><?php the_title(); ?></h3>
+            <div class="causeContent">
+             <?php the_content(); ?>
+            </div>
+            <?php the_post_thumbnail('thumbnail',[ 'alt' => '']) ?>
         </div>
-        <?php the_post_thumbnail('thumbnail',[ 'alt' => '']) ?>
-    </div>
-  </div>
-</div>
-<?php endwhile; ?>
-</div>
-
-
-
-
-<div class="container">
-<h1>Conséquences</h1>
-
-<?php
-$consequence = get_posts(array(
-'post_type' => 'consequence',
-'showposts' => 3,
-'numberposts' => 3,
-'order' => 'ASC'
-) );
-$i = 0;
-while (have_posts()) : the_post();
- ?>
-<div class="causeDesktop">
- <?php
-
-foreach ($consequence as $post) {
-
-  $i++;
-  if ($i <=3){
-  if ($i % 2 != 0){
-?>
-<div class="row cause">
-  <div class="col-lg-6 col-sm-12">
-      <?php the_post_thumbnail('medium',[ 'alt' => '']) ?>
-  </div>
-  <div class="col-lg-6 col-sm-12">
-      <h3><?php the_title(); ?></h3>
-      <div class="causeContent">
-       <?php the_excerpt(); ?>
       </div>
-  </div>
-</div>
+    </div>
+  <?php endwhile; ?>
 
-<?php
-} else{
-  ?>
-  <div class="row cause">
-    <div class="col-lg-6 col-sm-12">
-        <h3><?php the_title(); ?></h3>
-        <div class="causeContent">
-         <?php the_excerpt(); ?>
+
+
+
+<div class="centerCause">
+  <div class="container">
+    <h1>Conséquences</h1>
+
+    <?php
+    $consequence = get_posts(array(
+    'post_type' => 'consequence',
+    'showposts' => 3,
+    'numberposts' => 3,
+    'order' => 'ASC'
+    ) );
+    $i = 0;
+    while (have_posts()) : the_post();
+     ?>
+    <div class="causeDesktop">
+     <?php
+
+    foreach ($consequence as $post) {
+
+      $i++;
+      if ($i <=3){
+      if ($i % 2 != 0){
+    ?>
+    <div class="row cause">
+      <div class="col-lg-6 col-sm-12">
+          <?php the_post_thumbnail('medium',[ 'alt' => '']) ?>
+      </div>
+      <div class="col-lg-6 col-sm-12 childcenter">
+          <h3><?php the_title(); ?></h3>
+          <div class="causeContent">
+           <?php the_excerpt(); ?>
+          </div>
+      </div>
+    </div>
+
+    <?php
+    } else{
+      ?>
+      <div class="row cause">
+        <div class="col-lg-6 col-sm-12">
+            <h3><?php the_title(); ?></h3>
+            <div class="causeContent">
+             <?php the_excerpt(); ?>
+            </div>
         </div>
+        <div class="col-lg-6 col-sm-12 childcenter">
+            <?php the_post_thumbnail('medium',[ 'alt' => '']) ?>
+        </div>
+      </div>
+    <?php
+    }
+    }
+    }
+    endwhile;
+    ?>
     </div>
-    <div class="col-lg-6 col-sm-12">
-        <?php the_post_thumbnail('medium',[ 'alt' => '']) ?>
-    </div>
-  </div>
-<?php
-}
-}
-}
-endwhile;
-?>
-</div>
 
-<?php
-query_posts(array(
-'post_type' => 'consequence',
-'showposts' => 3,
-'numberposts' => 3,
-'order' => 'ASC'
-) );
-while (have_posts()) : the_post();
- ?>
+    <?php
+    query_posts(array(
+    'post_type' => 'consequence',
+    'showposts' => 3,
+    'numberposts' => 3,
+    'order' => 'ASC'
+    ) );
+    while (have_posts()) : the_post();
+     ?>
+  </div>
 </div>
 <div class="causeResponsive">
-  <div class="row cause">
-    <div class="col-12">
-        <h3><?php the_title(); ?></h3>
-        <div class="causeContent">
-         <?php the_content(); ?>
-        </div>
-        <?php the_post_thumbnail('thumbnail',[ 'alt' => '']) ?>
+    <div class="row cause">
+      <div class="col-12">
+          <h3><?php the_title(); ?></h3>
+          <div class="causeContent">
+           <?php the_content(); ?>
+          </div>
+          <?php the_post_thumbnail('thumbnail',[ 'alt' => '']) ?>
+      </div>
     </div>
   </div>
-</div>
-<?php endwhile; ?>
-</div>
+  <?php endwhile; ?>
+  <!-- </div> -->
 
 <hr>
 
