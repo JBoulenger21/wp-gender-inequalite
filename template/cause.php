75a9530ea4ead117def4dd755df75ab22a3e1a77
JBoulenger21 <boulenger.julie@gmail.com>
2020-10-29 11:31:16 +0100

    résolution conflits

--- template/cause.php
+++ template/cause.php
@@ -4,37 +4,7 @@ Template Name: Causes
 */
 get_header();
 ?>
-<div class="container">
-<h1>Causes</h1>
 
-<<<<<<< HEAD
-<?php
-  $cause = get_posts(array(
-  'post_type' => 'cause',
-  'showposts' => 3,
-  'numberposts' => 3,
-  'order' => 'ASC'
-  ) );
-$i = 0;
-while (have_posts()) : the_post();
- ?>
-<div class="causeDesktop">
- <?php
-
-foreach ($cause as $post) {
-
-  $i++;
-  if ($i % 2 == 0){
-?>
-<div class="row cause">
-  <div class="col-lg-6 col-sm-12">
-      <?php the_post_thumbnail('medium',[ 'alt' => '']) ?>
-  </div>
-  <div class="col-lg-6 col-sm-12">
-      <h3><?php the_title(); ?></h3>
-      <div class="causeContent">
-       <?php the_excerpt(); ?>
-=======
 <div class="centerCause">
   <div class="container">
     <div class="encadre-legis">
@@ -69,7 +39,6 @@ foreach ($cause as $post) {
           <div class="causeContent">
            <?php the_excerpt(); ?>
           </div>
->>>>>>> features/Julie
       </div>
   </div>
 </div>
@@ -121,16 +90,9 @@ while (have_posts()) : the_post();
 </div>
 
 
-<<<<<<< HEAD
-=======
 <div class="centerCause">
   <div class="container">
     <h2>Conséquences</h2>
->>>>>>> features/Julie
-
-
-<div class="container">
-<h1>Conséquences</h1>
 
 <?php
 $consequence = get_posts(array(
