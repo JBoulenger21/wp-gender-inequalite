5914360d0a6f45fe10be84f83e9b72f13f4f3fe6
mathieuradjouki <m.radjouki@codeur.online>
2020-10-30 10:15:59 +0100

    commit avant pull

--- footer.php
+++ footer.php
@@ -1,32 +1,29 @@
-<div class="footer">
+  <div class="footer">
 
-  <div class="container">
+    <div class="container">
 
-    <div class="footer-navigation">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a>
-            <?php wp_nav_menu( array (
-            'theme_location' => 'footer-hf',
-            'menu_class' => 'menu_navigation',
-          ) ); ?>
+      <div class="footer-navigation">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a>
+              <?php wp_nav_menu( array (
+              'theme_location' => 'footer-hf',
+              'menu_class' => 'menu_navigation',
+            ) ); ?>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="d-flex justify-content-center">
+        <div class="logo-footer">
+          <a href="<?php echo home_url( '/' ); ?>">
+            <?php echo the_custom_logo()?>
           </a>
-        </li>
-      </ul>
-    </div>
-
-    <div class="d-flex justify-content-center">
-      <div class="logo-footer">
-        <a href="<?php echo home_url( '/' ); ?>">
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.white.png" alt="icone logo">
-        </a>
+        </div>
       </div>
     </div>
   </div>
-
-</div>
-
-
     <?php wp_footer(); ?>
   </body>
 </html>
